d73b9cdde30d05b5a95c3f2a5a56958147312618
fangl <fangl@keyayun.com>
2019-07-22 15:12:09 +0800

    配置主题色

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,21 @@
 const {
     override,
     fixBabelImports,
-    // addLessLoader
+    addLessLoader
 } = require('customize-cra')
 
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: "es",
-        style: 'css',
-        // style: true
+        // style: 'css',
+        style: true
     }),
-    // 如果使用less
-    // addLessLoader({
-    //     javascriptEnabled: true
-    // })
+    // 如果使用less-loader对源码中对less进行覆盖
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#1DA57A'
+        },
+    })
 )
